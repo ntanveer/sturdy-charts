1dfc867fecc6bd4dd3913d331805ed92cff47ea8
Nabeel Tanveer <Nabeel.Tanveer@EYC.COM>
2016-05-13 16:33:30 +0100

    added scatter chart

--- js/app.js
+++ js/app.js
@@ -1,67 +1,76 @@
 var keyValueData, barData, lineData;
 
 function visualizePie() {
-    var pieChart = SC.chart.pie();
+	var pieChart = SC.chart.pie();
 		var pieElement = $('#pie-container');
 		pieChart
-			.data(keyValueData)
-			.height(250)
-			.width(500)
-            .options({ valueKey : 'taxable' });
+		.data(keyValueData)
+		.height(250)
+		.width(500)
+		.options({ valueKey: 'taxable' });
 		if (pieElement) {
-			pieChart(pieElement[0]);
+		pieChart(pieElement[0]);
 		}
 };
 
 function visualizeBar() {
   var barChart = SC.chart.bar();
-    var barElement = $('#bar-container');
-    
-    barChart
-			.data(barData)
-			.height(130)
-			.width(300)
-			.color(d3.scale.category20());
+	var barElement = $('#bar-container');
+
+	barChart
+		.data(barData)
+		.height(130)
+		.width(300)
+		.color(d3.scale.category20());
 
 		barChart(barElement[0]);
 }
 
 function generateBarData(d) {
-		var newData = $.map(d.values, function(o) {
-			return {
-				key : o.key,
-				value : o.values,
-				label: o.values
-			};
-		}).sort(function(a, b) {
-			return d3.ascending(a.key, b.key);
+		var newData = $.map(d.values, function (o) {
+		return {
+			key: o.key,
+			value: o.values,
+			label: o.values
+		};
+		}).sort(function (a, b) {
+		return d3.ascending(a.key, b.key);
 		});
 		return newData;
 };
 
-$( document ).ready(function() {
-    
-    d3.json("../data/products.json", function(error, json) {
-      if (error) return console.warn(error);
-      keyValueData = { values: d3.nest()
-        .key(function(d) { return d.taxable;})
-        .rollup(function(d) {
-          return d3.sum(d, function(g) {return g.price; });
-        }).entries(json)};
-      
-      visualizePie();
-        
-      barData = generateBarData(keyValueData);
-      visualizeBar();
-    });
-    
-    d3.json("../data/monthlySales.json", function(error, json) { 
-      var lineChart = SC.chart.line();
-			lineChart
-        .data(json)
-				.height(300)
-				.width(600);
+$(document).ready(function () {
 
-			lineChart("#line-container");
-    });
+	d3.json("../data/products.json", function (error, json) {
+		if (error) return console.warn(error);
+		keyValueData = {
+			values: d3.nest()
+        .key(function (d) { return d.taxable; })
+        .rollup(function (d) {
+          return d3.sum(d, function (g) { return g.price; });
+        }).entries(json)
+		};
+
+		visualizePie();
+
+		barData = generateBarData(keyValueData);
+		visualizeBar();
+	});
+
+	d3.json("../data/monthlySales.json", function (error, json) {
+		var lineChart = SC.chart.line();
+		lineChart
+			.data(json)
+			.height(350)
+			.width(400);
+
+		var scatterChart = SC.chart.scatter();
+		scatterChart
+			.data(json)
+			.height(350)
+			.width(400);
+
+		scatterChart("#scatter-container");
+		lineChart("#line-container");
+	});
 });
---
+++ src/charts/scatter/chart.js
@@ -0,0 +1,89 @@
+﻿var SC = SC || {};
+if (!SC.chart) { SC.chart = {}; }
+
+SC.chart.scatter = function () {
+
+	var chartData, chartWidth, chartHeight;
+
+	function chart(elementSelector) {
+		var svg = d3.select(elementSelector).append("svg");
+		chart.draw(svg);
+	}
+
+	chart.data = function (data) {
+		chartData = data;
+		return chart;
+	};
+
+	chart.height = function (value) {
+		chartHeight = value;
+		return chart;
+	};
+
+	chart.width = function (value) {
+		chartWidth = value;
+		return chart;
+	};
+
+	chart.draw = function (svg) {
+		//KPI color
+		function salesKPI(d) {
+			if (d >= 250) { return "#33CC66"; } else
+				if (d < 250) { return "#666666"; }
+		}
+
+		//create our SVG
+		svg.attr({ width: chartWidth, height: chartHeight });
+
+		//add min/max to array
+		Array.max = function (array) {
+			return Math.max.apply(Math, array);
+		};
+
+		Array.min = function (array) {
+			return Math.min.apply(Math, array);
+		};
+
+		//function for showing labels
+		function showMinMax(ds, col, val, type) {
+			var max = d3.max(ds, function (d) { return d[col]; });
+			var min = d3.min(ds, function (d) { return d[col]; });
+
+			if (type == 'minmax' && (val == max || val == min)) {
+				return val;
+			} else
+
+				if (type == 'all') {
+					return val;
+				}
+
+		}
+		//add dots
+		var dots = svg.selectAll("circle")
+			.data(chartData)
+			.enter()
+			.append("circle")
+			.attr({
+				cx: function (d) { return d.month * 3; },
+				cy: function (d) { return chartHeight - d.sales; },
+				r: 5,
+				"fill": function (d) { return salesKPI(d.sales); }
+			});
+
+		var labels = svg.selectAll("text")
+			.data(chartData)
+			.enter()
+			.append("text")
+			.text(function (d) { return showMinMax(chartData, 'sales', d.sales, 'minmax'); })
+			.attr({
+				x: function (d) { return (d.month * 3) - 25; },
+				y: function (d) { return chartHeight - d.sales; },
+				"font-size": "12px",
+				"font-family": "sans-serif",
+				"fill": "#666666",
+				"text-anchor": "start"
+			});
+	};
+
+	return chart;
+};
